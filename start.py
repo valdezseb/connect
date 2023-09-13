d1b1bb5f50ce29d393dcb20e752a7db87e1c43b6
valdezseb <111101968+valdezseb@users.noreply.github.com>
2023-09-13 09:14:43 -0700

    Update start.py

--- start.py
+++ start.py
@@ -1,3 +1,13 @@
-import streamlit as st
 import numpy as np
 import pandas as pd
+
+
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+url = "https://docs.google.com/spreadsheets/d/1da3hg3H0oSdii7zMxXL21jHVkKL2TByo4WJpk_MOyp8/edit?usp=sharing"
+
+conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+
+data = conn.read(spreadsheet=url, usecols=[0, 5])
+st.dataframe(data)
